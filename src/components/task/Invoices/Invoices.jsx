1627039fd0447b8ca935325dcb235dfd8960c0b7
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-05-02 18:37:06 +0600

    added invoices table

--- src/components/task/Invoices/Invoices.jsx
+++ src/components/task/Invoices/Invoices.jsx
@@ -1,7 +1,104 @@
+// components
+import { Card, Col, Row } from "react-bootstrap";
+import Table from "../../../components/Table";
+
+//dummy data
+import { records as data } from "../../../pages/tables/data";
+
+const columns = [
+  {
+    Header: "#",
+    accessor: "id",
+    sort: true,
+  },
+  {
+    Header: "Barcode",
+    sort: false,
+  },
+  {
+    Header: "Client Code",
+    sort: true,
+  },
+  {
+    Header: "Client Name",
+    accessor: "name",
+    sort: true,
+  },
+  {
+    Header: "Subtotal",
+    sort: true,
+  },
+  {
+    Header: "Discount",
+    sort: true,
+  },
+  {
+    Header: "Total",
+    sort: true,
+  },
+  {
+    Header: "Paid",
+    sort: true,
+  },
+  {
+    Header: "Due",
+    sort: true,
+  },
+  {
+    Header: "Date",
+    sort: true,
+  },
+  {
+    Header: "Status",
+    sort: false,
+  },
+  {
+    Header: "Action",
+    sort: false,
+  },
+];
+
+const sizePerPageList = [
+  {
+    text: "5",
+    value: 5,
+  },
+  {
+    text: "10",
+    value: 10,
+  },
+  {
+    text: "25",
+    value: 25,
+  },
+  {
+    text: "All",
+    value: data.length,
+  },
+];
+
 const Invoices = () => {
   return (
-    <div>
-      <h4>Invoices page will be shown here.</h4>
+    <div className="mt-3">
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <h4 className="header-title mb-3">Invoices Table</h4>
+
+              <Table
+                columns={columns}
+                data={data}
+                pageSize={5}
+                sizePerPageList={sizePerPageList}
+                isSortable={true}
+                pagination={true}
+                isSearchable={true}
+              />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
