3b637f07fcb04c2ed9eb6524bb902c8300d65484
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-05-04 16:44:40 +0600

    added tests and cultures table to the invoice page

--- src/components/task/Invoices/InvoiceCreate/InvoiceCreate.jsx
+++ src/components/task/Invoices/InvoiceCreate/InvoiceCreate.jsx
@@ -1,7 +1,55 @@
-import { Button, Card, Col, Form, Row } from "react-bootstrap";
+import { Card, Col, Form, Row } from "react-bootstrap";
 import PageTitle from "../../../PageTitle";
 import { useState } from "react";
 import "./InvoiceCreate.css";
+
+import Table from "../../../../components/Table";
+
+//dummy data
+import { records as data } from "../../../../pages/tables/data";
+
+const columns = [
+  {
+    Header: "Test Name",
+    accessor: "name",
+    sort: true,
+  },
+  {
+    Header: "Price",
+    sort: true,
+  },
+];
+
+const columns1 = [
+  {
+    Header: "Culture Name",
+    accessor: "name",
+    sort: true,
+  },
+  {
+    Header: "Price",
+    sort: true,
+  },
+];
+
+const sizePerPageList = [
+  {
+    text: "5",
+    value: 5,
+  },
+  {
+    text: "10",
+    value: 10,
+  },
+  {
+    text: "25",
+    value: 25,
+  },
+  {
+    text: "All",
+    value: data.length,
+  },
+];
 
 const InvoiceCreate = () => {
   const [validated, setValidated] = useState(false);
@@ -39,6 +87,7 @@ const InvoiceCreate = () => {
           validated={validated}
           onSubmit={handleSubmit}
         >
+          {/* select branches */}
           <Card>
             <Card.Body>
               <h4 className="header-title mt-0 mb-3">Select Branch</h4>
@@ -54,6 +103,7 @@ const InvoiceCreate = () => {
               </Row>
             </Card.Body>
           </Card>
+          {/* patient info */}
           <Card>
             <Card.Body>
               <h4 className="header-title mt-0 mb-1">Patient Info</h4>
@@ -133,6 +183,49 @@ const InvoiceCreate = () => {
               </Row>
             </Card.Body>
           </Card>
+          <Row>
+            <div className="col-lg-6">
+              <Col>
+                <Card>
+                  <Card.Body>
+                    <h4 className="header-title mt-0 mb-1">Tests</h4>
+                    <Row>
+                      <Table
+                        columns={columns}
+                        data={data}
+                        pageSize={5}
+                        sizePerPageList={sizePerPageList}
+                        isSortable={true}
+                        pagination={true}
+                        isSearchable={true}
+                      />
+                    </Row>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </div>
+
+            <div className="col-lg-6">
+              <Col>
+                <Card>
+                  <Card.Body>
+                    <h4 className="header-title mt-0 mb-1">Cultures</h4>
+                    <Row>
+                      <Table
+                        columns={columns1}
+                        data={data}
+                        pageSize={5}
+                        sizePerPageList={sizePerPageList}
+                        isSortable={true}
+                        pagination={true}
+                        isSearchable={true}
+                      />
+                    </Row>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </div>
+          </Row>
         </Form>
       </div>
     </>
